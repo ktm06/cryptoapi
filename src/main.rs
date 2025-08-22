7b96d66557d5949a3cab6bea3c6c22cde68d091b
ktm06 <mikikippei@gmail.com>
2025-08-22 17:37:22 -0400

    .

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 use actix_web::{App, HttpServer};
 use actix_web::middleware::Logger;
 
-<<<<<<< HEAD
 pub mod utils;
 pub mod routes;
-=======
-mod utils;
-mod routes;
->>>>>>> 815da385e2c8327b9a34c7ab304ecc33e84ee1e0
 
 
-
+//fixed
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
